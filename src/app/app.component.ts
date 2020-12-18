bd6bad4336765ea33dcf173e47b3dfa28ac20e10
Decabra <sarmadcute83@gmail.com>
2020-12-19 00:26:20 +0500

    to do list app integrated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'todo';
+  taskArray: string[] = [];
+  taskName = '';
+
+  addTask(): void{
+    if (this.taskName !== ''){
+      this.taskArray.push(this.taskName);
+      this.taskName = '';
+    }
+    else{
+      alert('Field is empty!');
+    }
+  }
+  removeTask(index: number): void{
+    this.taskArray.splice(index, 1);
+  }
 }
